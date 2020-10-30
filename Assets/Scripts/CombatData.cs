65cd329f7b8486aad42092dbaabc916a179ed981
Daniel Jaska <dtj190000@utdallas.edu>
2020-10-30 17:50:20 -0500

    V01

---
+++ Assets/Scripts/CombatData.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatData : MonoBehaviour
+{
+    [SerializeField] string _entityName;
+    public string getEntityName { get { return _entityName; } }
+    [SerializeField] int _attackValue;
+    public float getAttackValue { get { return _attackValue; } }
+    [SerializeField] int _defenseValue;
+    public float getDefenseValue { get { return _defenseValue; } }
+
+    [SerializeField] int _currentHp;
+    public int getCurrentHp { get { return _currentHp; } }
+    [SerializeField] int _maxHp;
+    public int getMaxHp { get { return _maxHp; } }
+
+    [SerializeField] int _minDamageValue;
+    public int getMinDamageValue { get { return _minDamageValue; } }
+    [SerializeField] int _maxDamageValue;
+    public int getMaxDamageValue { get { return _maxDamageValue; } }
+    [SerializeField] float _attackSpeed;
+    public float getAttackSpeed { get { return _attackSpeed; } }
+
+    public void TakeDamage(int damage)
+    {
+        _currentHp -= damage;
+        if(_currentHp < 0)
+        {
+            _currentHp = 0;
+        }
+    }
+
+}
---
+++ Assets/Scripts/CombatManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatManager : MonoBehaviour
+{
+    [SerializeField] PlayerData player;
+    [SerializeField] EnemyData enemy;
+
+    [SerializeField] Coroutine playerCoroutine;
+    [SerializeField] Coroutine enemyCoroutine;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerCoroutine = StartCoroutine(BeginCombat(player, enemy));
+        enemyCoroutine = StartCoroutine(BeginCombat(enemy, player));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator BeginCombat(CombatData entityAttacker, CombatData entityDefender)
+    {
+        while(entityDefender.getCurrentHp > 0)
+        {
+            float checkHit = entityAttacker.getAttackValue / entityDefender.getDefenseValue;
+
+            float rand = Random.Range(0f, 1f);
+
+            if (checkHit > .9f)
+            {
+                checkHit = .9f;
+            }
+            else if (checkHit < .05f)
+            {
+                checkHit = .05f;
+            }
+
+            if (checkHit >= rand)
+            {
+                entityDefender.TakeDamage(Random.Range(entityAttacker.getMinDamageValue, entityAttacker.getMaxDamageValue));
+                Debug.Log(entityDefender.getEntityName + " HP: " + entityDefender.getCurrentHp + " / " + entityDefender.getMaxHp + " and took ");
+
+                if(entityDefender.getCurrentHp <= 0)
+                {
+                    Debug.Log(entityAttacker.getEntityName + " wins!");
+                    if(entityAttacker.getEntityName == player.getEntityName)
+                    {
+                        StopCoroutine(enemyCoroutine);
+                        StopCoroutine(playerCoroutine);
+                    }
+                    else
+                    {
+                        StopCoroutine(playerCoroutine);
+                        StopCoroutine(enemyCoroutine);
+                    }
+                }
+            }
+            else
+            {
+                Debug.Log(entityAttacker.getEntityName + " missed.");
+            }
+
+            yield return new WaitForSeconds(entityAttacker.getAttackSpeed);
+        }
+        
+    }
+}
